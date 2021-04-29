137f7429581d4b40d5c2e6ac6c36e98cfb21ccf7
banfanci <bflobos@uc.cl>
2021-04-29 13:03:18 -0400

    try ssh 2

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const coursesController = require('../controllers/coursesController');
 
 const router = express.Router();
+
 router.route('/').get(coursesController.getCourses);
+
 router.route('/vacancy').get(coursesController.getVacancy);
+
 router.route('/requeriments').get(coursesController.getRequeriments);
 
 module.exports = router;
